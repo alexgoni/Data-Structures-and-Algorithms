4aabe1e4e313dc9181a6ad26145a2a31d7991074
alexgoni <goni000211@gmail.com>
2024-09-16 15:44:21 +0900

    feat: Graph BFS & DFS

--- data-structures/Graph.js
+++ data-structures/Graph.js
@@ -29,4 +29,82 @@ class Graph {
 
     delete this.adjacencyList[vertex];
   }
+
+  BFS(start) {
+    const result = [];
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+
+    while (queue.length > 0) {
+      const currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  DFSR(start) {
+    const result = [];
+    const visited = {};
+
+    const helper = (vertex) => {
+      if (!vertex) return null;
+
+      visited[vertex] = true;
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) return helper(neighbor);
+      });
+    };
+
+    helper(start);
+    return result;
+  }
+
+  DFSI(start) {
+    const result = [];
+    const stack = [start];
+    const visited = {};
+    visited[start] = true;
+
+    while (stack.length > 0) {
+      const currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
 }
+
+const g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
